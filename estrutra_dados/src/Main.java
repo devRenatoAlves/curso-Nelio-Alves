60143ef751e50fccfa2e3cec8a7c7cca663f22ad
Renato Alves <devrenatoalves@gmail.com>
2024-03-25 22:16:57 -0300

    First commit

---
+++ estrutra_dados/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		/*double x;
+		x = sc.nextDouble();*/
+		
+		/*char x;
+		x = sc.next().charAt(0);*/
+		
+		/*int x;
+		x = sc.nextInt();*/
+		
+		String x;
+		x = sc.nextLine();
+		
+		System.out.println("Você digitou: " + x);
+		
+		
+		sc.close();
+
+	}
+
+}
